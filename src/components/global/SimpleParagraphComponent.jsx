10844c3ddf029b83d1d37dd67f7f39c18ed47559
Mereck Fandohan <fandohan@ad.unc.edu>
2026-03-19 22:41:10 -0400

    completed base work through services page

--- src/components/global/SimpleParagraphComponent.jsx
+++ src/components/global/SimpleParagraphComponent.jsx
@@ -1,6 +1,6 @@
 function SimpleParagraphComponent({ text, children, className }) {
     return (
-        <p className={`text-center text-base w-[85vw] mx-auto mb-4 ${className || ''}`}>{text || children}</p>
+        <div className={`text-center text-base w-[85vw] mx-auto mb-4 ${className || ''}`}>{text || children}</div>
     )
 }
 
--- src/components/services-page/ServicesPageComponent.jsx
+++ src/components/services-page/ServicesPageComponent.jsx
@@ -1,7 +1,76 @@
+import DividerImageComponent from "../global/DividerImageComponent";
+import SectionHeaderComponent from "../global/SectionHeaderComponent";
+import SimpleParagraphComponent from "../global/SimpleParagraphComponent";
+import servicesImage1 from "@/assets/service-card-2.png";
+import servicesImage2 from "@/assets/service-card-3.webp";
+import servicesImage3 from "@/assets/about-us-image-2.webp";
 function ServicesPageComponent() {
     return (
         <div>
-
+            <div className="mx-6">
+                <SectionHeaderComponent text="Services We Offer" className="mb-6" />
+                <SectionHeaderComponent text="🏠 Residential Support & Supervision" className="text-left text-xl" />
+                <SimpleParagraphComponent className="text-left text-lg mb-4 ">
+                    <ul className="list-disc list-inside">
+                        <li>24-hour staff support</li>
+                        <li>assistance with daily routines</li>
+                        <li>safety monitoring</li>
+                    </ul>
+                </SimpleParagraphComponent>
+                <SectionHeaderComponent text="🧠 Rehabilitation & Skill Development" className="text-left text-xl" />
+                <SimpleParagraphComponent className="text-left text-lg mb-4 ">
+                    <ul className="list-disc list-inside">
+                        <li>life skills training</li>
+                        <li>communication + social skills</li>
+                        <li>building independence</li>
+                    </ul>
+                </SimpleParagraphComponent>
+            </div>
+            <div>
+                <DividerImageComponent imageSrc={servicesImage1} altText="Two adults working on a birdhouse." className="my-6" />
+            </div>
+            <div className="mx-6">
+                <SectionHeaderComponent text="🏥 Health & Wellness Support" className="text-left text-xl" />
+                <SimpleParagraphComponent className="text-left text-lg mb-4 ">
+                    <ul className="list-disc list-inside">
+                        <li>coordination with doctors</li>
+                        <li>medication support</li>
+                        <li>appointments + health tracking</li>
+                    </ul>
+                </SimpleParagraphComponent>
+                <SectionHeaderComponent text="💬 Behavioral & Emotional Support" className="text-left text-xl" />
+                <SimpleParagraphComponent className="text-left text-lg mb-4 ">
+                    <ul className="list-disc list-inside">
+                        <li>behavior support plans</li>
+                        <li>coping strategies</li>
+                        <li>coordination with professionals</li>
+                    </ul>
+                </SimpleParagraphComponent>
+            </div>
+            <div>
+                <DividerImageComponent imageSrc={servicesImage2} altText="A disabled adult being checked up on by a doctor/nurse." className="my-6" />
+            </div>
+            <div className="mx-6">
+                <SectionHeaderComponent text="🌍 Community Integration" className="text-left text-xl" />
+                <SimpleParagraphComponent className="text-left text-lg mb-4 ">
+                    <ul className="list-disc list-inside">
+                        <li>recreational activities</li>
+                        <li>social engagement</li>
+                        <li>community participation</li>
+                    </ul>
+                </SimpleParagraphComponent>
+                <SectionHeaderComponent text="⚖️ Rights & Advocacy" className="text-left text-xl" />
+                <SimpleParagraphComponent className="text-left text-lg mb-4 ">
+                    <ul className="list-disc list-inside">
+                        <li>protecting client rights</li>
+                        <li>promoting independence</li>
+                        <li>access to advocacy resources</li>
+                    </ul>
+                </SimpleParagraphComponent>
+            </div>
+            <div>
+                <DividerImageComponent imageSrc={servicesImage3} altText="Two adults sitting on a couch." className="my-6" />
+            </div>
         </div>
     )
 
